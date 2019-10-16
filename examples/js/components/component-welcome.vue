ca059bb96dafa2f5fee203e9cb60c7869015d2a7
Hendrik Putzek <hendrik@putzek.com>
2019-10-17 00:12:21 +0200

    Add link to github repo

--- examples/js/components/component-welcome.vue
+++ examples/js/components/component-welcome.vue
@@ -1,20 +1,31 @@
 <template>
-    <h1>Testing tool for s+pixels protocol</h1>
+  <h1>
+    Testing tool for
+    <a
+      href="https://github.com/hputzek/little-stream-protocol"
+      title="Show on github"
+      >pixels+s protocol</a
+    >
+  </h1>
 </template>
 
 <script>
-    module.exports = {
-        data() {
-            return {
-                message: 'default value'
-            }
-        },
-        methods: {
-
-        }
-    }
+module.exports = {
+  data() {
+    return {
+      message: "default value"
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style scoped>
-
+h1 a {
+  text-decoration: none;
+  color: dodgerblue;
+  display: inline-block;
+  padding-bottom: 3px;
+  border-bottom: 1px dotted dodgerblue;
+}
 </style>
